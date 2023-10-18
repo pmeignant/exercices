bb7dfb446b2ce36de776eabaad3d3dfd9f548742
pierrickMeignant <pierrick.meignant@ext.adeo.com>
2023-10-18 14:53:25 +0200

    rename exercice sort

--- src/main/java/exerciceThree/Sort.java
+++ src/main/java/exerciceThree/Sort.java
@@ -1,6 +1,5 @@
 package exerciceThree;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -8,11 +7,11 @@ import java.util.Objects;
 public class Sort {
 
     /**
-     * tri de la liste
+     * tri fusion de la liste
      * @param vals liste d'entier
      * @return liste d'entier trié en ordre croissant
      */
-    private static List<Integer> sort(List<Integer> vals) {
+    private static List<Integer> mergeSort(List<Integer> vals) {
         return List.of();
     }
 
@@ -26,14 +25,14 @@ public class Sort {
         List<Integer> dataTest4 = List.of(9, -245, 46, -193, 32, -12, 3, 73, 225, -11, -47, -11);
 
         System.out.println("****** TESTS TRI ******");
-        final List<Integer> result1 = sort(dataTest1);
+        final List<Integer> result1 = mergeSort(dataTest1);
         final String expected1 = "[]";
         System.out.println("List 1 { " +
                 "result: " + result1 + "," +
                 " expected: " + expected1 +
                 ", status: " + (Objects.equals(result1.toString(), expected1) ? "SUCCEED" : "FAILED") +
                 "}");
-        final List<Integer> result2 = sort(dataTest2);
+        final List<Integer> result2 = mergeSort(dataTest2);
         final String expected2 = "[]";
         System.out.println("List 2 { " +
                 "result: " + result2 + "," +
@@ -41,7 +40,7 @@ public class Sort {
                 ", status: " + (Objects.equals(result2.toString(), expected2) ? "SUCCEED" : "FAILED") +
                 "}");
 
-        final List<Integer> result3 = sort(dataTest3);
+        final List<Integer> result3 = mergeSort(dataTest3);
         final String expected3 = "[3, 4, 7, 8, 10, 32, 36, 42, 73, 200]";
         System.out.println("List 3 { " +
                 "result: " + result3 + "," +
@@ -49,7 +48,7 @@ public class Sort {
                 ", status: " + (Objects.equals(result3.toString(), expected3) ? "SUCCEED" : "FAILED") +
                 "}");
 
-        final List<Integer> result4 = sort(dataTest4);
+        final List<Integer> result4 = mergeSort(dataTest4);
         final String expected4 = "[-245, -193, -47, -12, -11, -11, 3, 9, 32, 46, 73, 225]";
         System.out.println("List 4 { " +
                 "result: " + result4 + "," +
